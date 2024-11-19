4043bfec1da13da26cb769e675ad4f7c02ceb660
Plamen <plamengeorgiev967@gmail.com>
2024-11-19 17:34:44 +0200

    Adding examples for components and pages

---
+++ src/app/components/ExampleComp.jsx
@@ -0,0 +1,5 @@
+export default function ExampleComp() {
+  return (
+    <div>Example Component</div>
+  )
+}
---
+++ src/app/pages/ExamplePage.jsx
@@ -0,0 +1,5 @@
+export default function ExamplePage() {
+  return (
+    <div>ExamplePage</div>
+  )
+}
