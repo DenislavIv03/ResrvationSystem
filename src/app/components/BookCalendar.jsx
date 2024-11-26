220fcf2dccb04d75360ff4b095217ac583444f6f
Plamen <plamengeorgiev967@gmail.com>
2024-11-26 15:29:25 +0200

    Adding validation for reserved hours

--- src/app/components/BookCalendar.jsx
+++ src/app/components/BookCalendar.jsx
@@ -1,9 +1,10 @@
-import { Box, Button, Fade, Modal } from "@mui/material"
+import { Box, Button, Fade, Modal, Typography } from "@mui/material"
 import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers"
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { reserveDate, selectCalendarDates } from "../../features/slices/bookCalendarSlice"
+import dayjs from "dayjs"
 
 function BookCalendar() {
   
@@ -11,13 +12,33 @@ function BookCalendar() {
   const reservedDates = useSelector(selectCalendarDates)
 
   const [open, setOpen] = useState(false);
-  const [selectedDate,setSelectedDate] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [error, setError] = useState(null)
   
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const handlePickerChange = (newDate) => setSelectedDate(newDate);
+  const handleError = (newError) => setError(newError);
+
+  const shouldDisableDates = (dateForDisable) => {
+    return reservedDates.some(date =>
+         dateForDisable.isSame(dayjs(date),'day')
+    )
+  }
+
+  const shouldDisableTime = (timeForDisable,view) => {
+
+    if(view == 'hours' || view == 'seconds') return false;
+
+    if(timeForDisable.minute() !== 0 &&
+       timeForDisable.minute() !== 30) return true;
+
+    return reservedDates.some(date => dayjs(date).isSame(timeForDisable,'minute'))
+  }
 
   const handleSaveChanges = () => {
+    if(error) return;
+    
     if(selectedDate) {
         dispatch(reserveDate(selectedDate.toString()))
     }
@@ -30,15 +51,35 @@ function BookCalendar() {
         <Button onClick={handleOpen}>Open Modal</Button>
 
         <Modal
+         sx={{
+            width: 700,
+            maxWidth: 'calc(100% - 2.5rem)',
+            marginInline: 'auto',
+            marginBlock: 2,
+         }}
          open={open}
          onClose={handleClose}
+         closeAfterTransition
+         disableRestoreFocus
          >
             <Fade in={open} >
-                <Box sx={{bgcolor: "white"}}>
+                <Box
+                 sx={{
+                    bgcolor: "white",
+                    p: 2,
+                    borderRadius: 2
+                 }}>
+                    <Typography variant="h4" sx={{mb: 2,}}>Reserve Service</Typography>
                     <LocalizationProvider dateAdapter={AdapterDayjs}>
                         <DateTimePicker
+                            label="Select date and time"
+                            // shouldDisableDate={shouldDisableDates}
+                            shouldDisableTime={shouldDisableTime}
+                            showDaysOutsideCurrentMonth
+                            minDate={dayjs(new Date())}
                             value={selectedDate}
                             onChange={handlePickerChange}
+                            onError={handleError}
                         />
                     </LocalizationProvider>
                     <Button onClick={handleSaveChanges}>Save Changes</Button>
