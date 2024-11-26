8b1072e306f782607e058d0affc91424900ef165
sevinchrosa <sevinchreceb@gmail.com>
2024-11-26 02:13:01 +0100

    Added new page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import SignIn from './app/pages/SignIn'
 import ProtectedRoute from './app/components/common/ProtectedRoute'
 import { logout } from './features/slices/authSlice'
 import { useDispatch } from 'react-redux'
+import MyProfile from './app/pages/ProfilePage'
+import SignUp from './app/pages/Register'
 
 
 function App() {
@@ -18,6 +20,12 @@ function App() {
             <Box sx={{  marginRight: "10px"}}>Reservation System</Box>
             <div>I am logged in!</div>
             <button onClick={() => dispatch(logout())}>Logout</button>
+            <Routes>
+            <Route path="profile" element={<MyProfile/>} />
+            </Routes>
+            <Routes>
+            <Route path="register" element={<SignUp/>} />
+            </Routes>
           </ProtectedRoute>
         }/>
         <Route path='/sign-in' element={<SignIn />}/>
